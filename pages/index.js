ea69e928051e7148690966a228ab75f75c2f6b6a
Cole Rogers <colerogers@google.com>
2023-12-19 11:33:44 -0500

    add line

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Cole!</a>
+          This is a new line.
         </h1>
 
         <p className={styles.description}>
