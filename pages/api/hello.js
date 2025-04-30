057a7a2531551e1fde4e3ab530cff26d35db6350
Cole Rogers <colerogers@google.com>
2025-04-30 15:58:08 -0400

    adding ! after 'name'

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Cole Rogers' })
+  res.status(200).json({ name: 'Cole Rogers!' })
 }
