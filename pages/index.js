7879d64ede91626a848b92720c231f186d367b05
Cole Rogers <colerogers@google.com>
2025-06-09 15:48:00 -0400

    Changing 'Welcome to' to 'Greetings from'

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Coles site!</a>
+          Greetings from <a href="https://nextjs.org">Coles site!</a>
         </h1>
 
         <p className={styles.description}>
